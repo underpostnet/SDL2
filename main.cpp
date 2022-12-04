47ca357e44d0b23e298104e0006e1cf3d7cc634f
fcoverdugo <fcoverdugoa@underpost.net>
2022-12-04 10:24:51 -0300

    update

--- main.cpp
+++ main.cpp
@@ -3,31 +3,79 @@
 
 const int WIDTH = 800, HEIGHT = 600;
 
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-    SDL_Init( SDL_INIT_EVERYTHING );
+    SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *window = SDL_CreateWindow( "Hello SDL WORLD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI );
+    SDL_Window *window = SDL_CreateWindow("Hello SDL WORLD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
 
-    if ( NULL == window )
+    // renderer
+    SDL_Renderer *renderer = nullptr;
+
+    // this opens a font style and sets a size
+    TTF_Font *Sans = TTF_OpenFont("Sans.ttf", 24);
+
+    // this is the color in rgb format,
+    // maxing out all would give you the color white,
+    // and it will be your text's color
+    SDL_Color White = {255, 255, 255};
+
+    // as TTF_RenderText_Solid could only be used on
+    // SDL_Surface then you have to create the surface first
+    SDL_Surface *surfaceMessage =
+        TTF_RenderText_Solid(Sans, "put your text here", White);
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // now you can convert it into a texture
+    SDL_Texture *Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+    SDL_Rect Message_rect; // create a rect
+    Message_rect.x = 0;    // controls the rect's x coordinate
+    Message_rect.y = 0;    // controls the rect's y coordinte
+    Message_rect.w = 100;  // controls the width of the rect
+    Message_rect.h = 100;  // controls the height of the rect
+
+    // (0,0) is on the top left of the window/screen,
+    // think a rect as the text's box,
+    // that way it would be very simple to understand
+
+    // Now since it's a texture, you have to put RenderCopy
+    // in your game loop area, the area where the whole code executes
+
+    // you put the renderer's name first, the Message,
+    // the crop size (you can ignore this if you don't want
+    // to dabble with cropping), and the rect which is the size
+    // and coordinate of your texture
+    SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
+
+    // Don't forget to free your surface and texture
+    SDL_FreeSurface(surfaceMessage);
+    SDL_DestroyTexture(Message);
+
+    TTF_Init();
+
+    if (NULL == window)
     {
-        std::cout << "Could not create window: " << SDL_GetError( ) << std::endl;
+        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
         return 1;
     }
 
     SDL_Event windowEvent;
 
-    while ( true )
+    while (true)
     {
-        if ( SDL_PollEvent( &windowEvent ) )
+        if (SDL_PollEvent(&windowEvent))
         {
-            if ( SDL_QUIT == windowEvent.type )
-            { break; }
+            if (SDL_QUIT == windowEvent.type)
+            {
+                break;
+            }
         }
     }
 
-    SDL_DestroyWindow( window );
-    SDL_Quit( );
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
